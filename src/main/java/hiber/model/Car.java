8b81c017295c193087a0d2e5ec3a42bf581ff3a2
kalachev90 <kalachev-90@mail.ru>
2023-03-10 01:23:01 +0400

    Pre-project_2_2_1

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
